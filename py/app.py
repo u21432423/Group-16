07ec7ca4375ae50405b2c8770fd9a5a976398c21
Heinrich946 <u22611632@tuks.co.za>
2025-11-23 17:24:17 +0200

    Update project files

--- py/app.py
+++ py/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'    # Makes project.db in your folder
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
